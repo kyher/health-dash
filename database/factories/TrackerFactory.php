af474ff531f5b1f83a7609368996a7d7e5e9c317
kyher <kyle.heron@pm.me>
2026-04-07 19:12:38 +0100

    Add tests / factories

---
+++ database/factories/TrackerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tracker;
+use App\Models\TrackerCategory;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Tracker>
+ */
+class TrackerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'category_id' => TrackerCategory::factory(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
